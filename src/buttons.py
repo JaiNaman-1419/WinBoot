2c2867b8c92f59ec4b605420dd7cb80b506aba44
Naman Jain <Naman.Jain1419@gmail.com>
2021-08-03 01:06:19 +0530

    Completed definition of add_file method and remove file button. Also change_drivebtn_and_drivelabel_state method and change_addfile_and_filenamelabel_state method shifted from main file to buttons file.

--- src/buttons.py
+++ src/buttons.py
@@ -1,20 +1,36 @@
+from src.format import Format
+from PyQt5.QtWidgets import QFileDialog
+
+
 class Buttons:
 
     # Buttons of Main Window Dialog/
     # TODO define working of buttons in Main Window Dialog.
 
     def __init__(self, window):
-        self.__main_window = window
+        # Class Attributes
         self.__file_name = None
         self.__file_path = None
         self.__drive_name = None
         self.__drive_path = None
+        # Single reference of class MainWindow in main file.
+        self.__main_window = window
+        # Class objects
+        self.__format = Format()
 
     def add_iso_file(self):
-        pass
+        file = QFileDialog.getOpenFileName(self.__main_window, "Select .iso file", filter="ISO File(*.iso)")[0]
+        if file == '' or file is None:
+            print("Please select Windows iso file!")
+            return
+        self.__file_path, self.__file_name = self.__format.format_filepath(file)
+        self.change_addfile_and_filenamelabel_state()
+        self.__main_window.remove_file_button.show()
+        self.__main_window.drive_button.setDisabled(False)
+
 
     def select_usb_drive(self):
-        pass
+        self.__main_window.drive_frame.show()
 
     def start_flash_button(self):
         pass
@@ -23,7 +39,12 @@ class Buttons:
         pass
 
     def remove_file_button(self):
-        pass
+        self.__file_name = None
+        self.__file_path = None
+        self.__main_window.remove_file_button.hide()
+        self.__main_window.file_name_label.hide()
+        self.__main_window.add_button.show()
+        self.__main_window.drive_button.setEnabled(False)
 
     def change_drive_button(self):
         pass
@@ -32,7 +53,17 @@ class Buttons:
         pass
 
     def cancel_btn_in_drive_frame(self):
-        pass
+        self.__main_window.drive_frame.hide()
+
+    def change_addfile_and_filenamelabel_state(self):
+        self.__main_window.add_button.hide()
+        self.__main_window.file_name_label.setText(self.__format.format_string_label(self.__file_name))
+        self.__main_window.file_name_label.show()
+
+    def change_drivebtn_and_drivelabel_state(self):
+        self.__main_window.drive_button.hide()
+        self.__main_window.drive_name_label.setText(self.__format.format_string_label(self.__drive_name))
+        self.__main_window.drive_name_label.show()
 
     # Buttons of Flash Screen dialog.
     # TODO define working of buttons in Flash Screen Dialog.
