31042d4caabe642bdf599c3d800d26ad0e2e589d
Naman Jain <Naman.Jain1419@gmail.com>
2021-08-05 01:01:26 +0530

    Created and added usb_drive.py module which gets all the properties of usb drive.

---
+++ src/usb_drive.py
@@ -0,0 +1,57 @@
+from psutil import disk_partitions
+from subprocess import Popen, PIPE
+from re import match, search, split
+
+
+class DriveProperties:
+
+    def __init__(self):
+        self.__disks = dict()
+        self.__disk_count = None
+        self.__disk_names = dict()
+
+    def __get_usb_list(self):
+        pattern = r"/dev/sd[b-z]"
+        disk_list = disk_partitions()
+        for disk in disk_list:
+            if match(pattern, disk.device):
+                self.__disks[disk.device] = disk.mountpoint
+
+        self.__disk_count = len(self.__disks.keys())
+
+    def __get_usb_model(self):
+
+        if self.__disks is None:
+            print("Disk list is Empty!")
+            return
+
+        disk_number = 0
+        output, error = Popen(["pkexec", "sudo", "fdisk", "-l"], stdout=PIPE, stderr=PIPE).communicate()
+        device_model_list = output.decode('utf-8').split('\n')
+
+        for index, device in enumerate(device_model_list):
+            if disk_number < self.__disk_count:
+                for disk in list(self.__disks.keys())[disk_number:]:
+                    device_pattern = fr"Disk {disk[:-1]}: (.*B),"
+                    model_pattern = r"Disk model: (.*)"
+                    if search(device_pattern, device):
+                        disk_number += 1
+                        size = split(device_pattern, device)[1].strip()
+                        self.__disk_names[disk] = {f"{size}": f"{split(model_pattern, device_model_list[index+1])[1].strip()}"}
+                        break
+
+            else:
+                return
+
+    def get_disk_properties(self):
+        self.__get_usb_list()
+        self.__get_usb_model()
+
+    def get_disk_count(self):
+        return self.__disk_count
+
+    def get_disk_list(self):
+        return self.__disks
+
+    def get_disk_names_list(self):
+        return self.__disk_names
