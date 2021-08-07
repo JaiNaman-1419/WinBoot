d7365bb31ae4c445e1b374ab9ba641601a1d660a
Naman Jain <Naman.Jain1419@gmail.com>
2021-08-07 16:16:24 +0530

    Changes in remove_file_button() method and in change_drive_button() method has been done. Definition of apply_btn_in_drive_frame() method completed.

--- src/buttons.py
+++ src/buttons.py
@@ -31,20 +31,46 @@ class Buttons:
         self.__main_window.remove_file_button.show()
         self.__main_window.drive_button.setDisabled(False)
 
+    def remove_file_button(self):
+        self.__file_name = None
+        self.__file_path = None
+        self.__drive_name = None
+        self.__drive_path = None
+        self.__main_window.remove_file_button.hide()
+        self.__main_window.change_drive_button.hide()
+        self.__main_window.file_name_label.hide()
+        self.__main_window.drive_name_label.hide()
+        self.__main_window.add_button.show()
+        self.__main_window.drive_button.show()
+        self.__main_window.drive_button.setEnabled(False)
+        self.__main_window.start_button.setEnabled(False)
+
     def select_usb_drive(self):
         self.__main_window.drive_frame.show()
         self.refresh_drive_table()
+
+    def change_drive_button(self):
+        self.__drive_name = None
+        self.__drive_path = None
+        self.__main_window.change_drive_button.hide()
+        self.__main_window.drive_name_label.hide()
+        self.__main_window.drive_button.show()
+        self.__main_window.apply_button.setEnabled(False)
+        self.__main_window.start_button.setEnabled(False)
 
     def __assign_drive_properties_in_table(self):
         self.__main_window.drive_table.setRowCount(self.__drive.get_disk_count())
         index = 0
         for location, value in self.__drive.get_disk_names_list().items():
             for size, name in value.items():
+                if name == "Cruzer Blade":
+                    name = "Sandisk " + name
                 name = QTableWidgetItem(name)
-                name.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+                name.setFlags(Qt.ItemIsEnabled)
                 name.setCheckState(Qt.Unchecked)
                 name.device = location
                 self.__checkbox_list.append(name)
+                print(self.__checkbox_list)
                 self.__main_window.drive_table.setItem(index, 0, name)
                 self.__main_window.drive_table.setItem(index, 1, QTableWidgetItem(size))
                 self.__main_window.drive_table.setItem(index, 2, QTableWidgetItem(location))
@@ -53,13 +79,22 @@ class Buttons:
     def drive_selected(self, drive):
         self.__drive_name = drive.text()
         self.__drive_path = drive.device
-        for disk in self.__checkbox_list:
-            if drive.device == disk.device:
-                continue
-            disk.setCheckState(Qt.Unchecked)
+        for index, disk in enumerate(self.__checkbox_list):
 
-    def start_flash_button(self):
-        pass
+            if drive.device == disk.device:
+                if disk.checkState() == Qt.Checked:
+                    disk.setCheckState(Qt.Unchecked)
+                    self.__drive_name = None
+                    self.__drive_path = None
+                    self.__main_window.apply_button.setEnabled(False)
+                else:
+                    disk.setCheckState(Qt.Checked)
+                    self.__main_window.apply_button.setDisabled(False)
+
+                print(f"Drive Name: {self.__drive_name}\nDrive Path: {self.__drive_path}")
+                continue
+
+            disk.setCheckState(Qt.Unchecked)
 
     def refresh_drive_table(self):
         if self.__main_window.drive_table.rowCount() > 0:
@@ -67,25 +102,27 @@ class Buttons:
             self.__drive.remove_dictionary_element()
             self.__drive.reduce_disk_count()
 
+        self.__checkbox_list.clear()
         self.__drive.get_disk_properties()
         self.__assign_drive_properties_in_table()
 
-    def remove_file_button(self):
-        self.__file_name = None
-        self.__file_path = None
-        self.__main_window.remove_file_button.hide()
-        self.__main_window.file_name_label.hide()
-        self.__main_window.add_button.show()
-        self.__main_window.drive_button.setEnabled(False)
-
-    def change_drive_button(self):
-        pass
-
     def apply_btn_in_drive_frame(self):
-        pass
+        self.__main_window.drive_frame.hide()
+        self.__main_window.drive_button.hide()
+        self.__main_window.drive_name_label.setText(self.__drive_name)
+        self.__main_window.drive_name_label.show()
+        self.__main_window.change_drive_button.show()
+        self.__main_window.start_button.setDisabled(False)
+        print(f"Drive Name: {self.__drive_name}\nDrive Path: {self.__drive_path}")
 
     def cancel_btn_in_drive_frame(self):
+        self.__drive_name = None
+        self.__drive_path = None
         self.__main_window.drive_frame.hide()
+        print(f"Drive Name: {self.__drive_name}\nDrive Path: {self.__drive_path}")
+
+    def start_flash_button(self):
+        pass
 
     def change_addfile_and_filenamelabel_state(self):
         self.__main_window.add_button.hide()
