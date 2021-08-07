a6601276cb6c3ee4cede4e0e2b2bfdc7471dc9ae
Naman Jain <Naman.Jain1419@gmail.com>
2021-08-07 22:28:14 +0530

    set_window_properties() method added and defined. Here, all properties of window are edited like its icon, size and its title.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from sys import argv, exit
 from PyQt5.uic import loadUi
+from functools import partial
+from PyQt5.QtGui import QIcon
 from src.buttons import Buttons
+from flash_screen import FlashScreen
 from src.usb_drive import DriveProperties
-from PyQt5.QtWidgets import QApplication, QDialog, QHeaderView  # , QStackedWidget
+from PyQt5.QtWidgets import QApplication, QDialog, QHeaderView, QStackedWidget
 
 
 class MainWindow(QDialog):
 
-    def __init__(self):
+    def __init__(self, widgets, drive):
         super(MainWindow, self).__init__()
 
         # Class objects or references
-        self.__drive = DriveProperties()
-        self.__button = Buttons(self, self.__drive)
+        self.__button = Buttons(widgets, drive)
 
         # Class attributes
         self.__file_name = None
@@ -39,15 +41,15 @@ class MainWindow(QDialog):
         self.change_drive_button.hide()
 
     def button_connectors(self):
-        self.add_button.clicked.connect(self.__button.add_iso_file)
-        self.drive_button.clicked.connect(self.__button.select_usb_drive)
-        self.start_button.clicked.connect(self.__button.start_flash_button)
-        self.refresh_button.clicked.connect(self.__button.refresh_drive_table)
-        self.apply_button.clicked.connect(self.__button.apply_btn_in_drive_frame)
-        self.remove_file_button.clicked.connect(self.__button.remove_file_button)
-        self.cancel_button.clicked.connect(self.__button.cancel_btn_in_drive_frame)
-        self.change_drive_button.clicked.connect(self.__button.change_drive_button)
-        self.drive_table.itemClicked.connect(self.__button.drive_selected)
+        self.add_button.clicked.connect(lambda: self.__button.add_iso_file(self))
+        self.drive_button.clicked.connect(lambda: self.__button.select_usb_drive(self))
+        self.start_button.clicked.connect(lambda: self.__button.start_flash_button())
+        self.refresh_button.clicked.connect(lambda: self.__button.refresh_drive_table(self))
+        self.apply_button.clicked.connect(lambda: self.__button.apply_btn_in_drive_frame(self))
+        self.remove_file_button.clicked.connect(lambda: self.__button.remove_file_button(self))
+        self.cancel_button.clicked.connect(lambda: self.__button.cancel_btn_in_drive_frame(self))
+        self.change_drive_button.clicked.connect(lambda: self.__button.change_drive_button(self))
+        self.drive_table.itemClicked.connect(partial(self.__button.drive_selected, self))
 
     def set_drive_table_properties(self):
         self.drive_table.setColumnWidth(0, 240)
@@ -56,9 +58,21 @@ class MainWindow(QDialog):
         self.drive_table.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
 
 
+def set_window_properties(widgets):
+    favicon = QIcon("./assets/favicon.png")
+    widgets.setWindowIcon(favicon)
+    widgets.setFixedSize(1024, 650)
+    widgets.setWindowTitle("WinBoot")
+
+
 if __name__ == '__main__':
     application = QApplication(argv)
-    # widgets = QStackedWidget()
-    main_window = MainWindow()
-    main_window.show()
+    widgets = QStackedWidget()
+    drive = DriveProperties()
+    main_window = MainWindow(widgets, drive)
+    flash_screen = FlashScreen(widgets, drive)
+    widgets.addWidget(main_window)
+    widgets.addWidget(flash_screen)
+    set_window_properties(widgets)
+    widgets.show()
     exit(application.exec())
