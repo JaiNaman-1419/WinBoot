c55816a8617ef2d2584bb06d08beac74e5d93e47
Naman Jain <Naman.Jain1419@gmail.com>
2021-10-12 19:05:28 +0530

    Removed iso file and drive attributes, getters & setters from main.py
    Also added one parameter to button connectors and MainWindow class for object of Data class.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.uic import loadUi
 from functools import partial
 from PyQt5.QtGui import QIcon
 from src.buttons import Buttons
+from Model.data_model import Data
 from flash_screen import FlashScreen
 from src.usb_drive import DriveProperties
 from PyQt5.QtWidgets import QApplication, QDialog, QHeaderView, QStackedWidget
@@ -10,17 +11,12 @@ from PyQt5.QtWidgets import QApplication, QDialog, QHeaderView, QStackedWidget
 
 class MainWindow(QDialog):
 
-    def __init__(self, widgets_obj, drive_obj):
+    def __init__(self, widgets_obj, drive_obj, data_obj):
         super(MainWindow, self).__init__()
 
         # Class objects or references
+        self.__data = data_obj
         self.__button = Buttons(widgets_obj, drive_obj)
-
-        # Class attributes
-        self.__file_name = None
-        self.__file_path = None
-        self.__drive_name = None
-        self.__drive_path = None
 
         # Loading UI
         loadUi("./ui/main_screen.ui", self)
@@ -41,15 +37,15 @@ class MainWindow(QDialog):
         self.change_drive_button.hide()
 
     def button_connectors(self):
-        self.add_button.clicked.connect(lambda: self.__button.add_iso_file(self))
+        self.add_button.clicked.connect(lambda: self.__button.add_iso_file(self, self.__data))
         self.drive_button.clicked.connect(lambda: self.__button.select_usb_drive(self))
-        self.start_button.clicked.connect(lambda: self.__button.start_flash_button())
+        self.start_button.clicked.connect(lambda: self.__button.start_flash_button(self.__data))
         self.refresh_button.clicked.connect(lambda: self.__button.refresh_drive_table(self))
-        self.apply_button.clicked.connect(lambda: self.__button.apply_btn_in_drive_frame(self))
-        self.remove_file_button.clicked.connect(lambda: self.__button.remove_file_button(self))
-        self.cancel_button.clicked.connect(lambda: self.__button.cancel_btn_in_drive_frame(self))
-        self.change_drive_button.clicked.connect(lambda: self.__button.change_drive_button(self))
-        self.drive_table.itemClicked.connect(partial(self.__button.drive_selected, self))
+        self.apply_button.clicked.connect(lambda: self.__button.apply_btn_in_drive_frame(self, self.__data))
+        self.remove_file_button.clicked.connect(lambda: self.__button.remove_file_button(self, self.__data))
+        self.cancel_button.clicked.connect(lambda: self.__button.cancel_btn_in_drive_frame(self, self.__data))
+        self.change_drive_button.clicked.connect(lambda: self.__button.change_drive_button(self, self.__data))
+        self.drive_table.itemClicked.connect(partial(self.__button.drive_selected, self, self.__data))
 
     def set_drive_table_properties(self):
         self.drive_table.setColumnWidth(0, 240)
@@ -57,46 +53,33 @@ class MainWindow(QDialog):
         self.drive_table.setColumnWidth(2, 180)
         self.drive_table.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
 
-    def set_file_name(self, filename):
-        self.__file_name = filename
 
-    def set_file_path(self, filepath):
-        self.__file_path = filepath
-
-    def set_drive_name(self, drive_name):
-        self.__drive_name = drive_name
-
-    def set_drive_path(self, drive_path):
-        self.__drive_path = drive_path
-
-    def get_file_name(self):
-        return self.__file_name
-
-    def get_file_path(self):
-        return self.__file_path
-
-    def get_drive_name(self):
-        return self.__drive_name
-
-    def get_drive_path(self):
-        return self.__drive_path
-
-
-def set_window_properties(widgets):
+def set_window_properties(widget):
     favicon = QIcon("./assets/favicon.png")
-    widgets.setWindowIcon(favicon)
-    widgets.setFixedSize(1024, 650)
-    widgets.setWindowTitle("WinBoot")
+    widget.setWindowIcon(favicon)
+    widget.setFixedSize(1024, 650)
+    widget.setWindowTitle("WinBoot")
 
 
 if __name__ == '__main__':
+
+    # Instance of QApplication class
     application = QApplication(argv)
+    # Instance of QStackedWidget class
     widgets = QStackedWidget()
+    # Instance of DriveProperties class
     drive = DriveProperties()
-    main_window = MainWindow(widgets, drive)
-    flash_screen = FlashScreen(widgets, drive)
+    # Instance of Data class
+    data = Data()
+    # Instance of MainWindow class from main.py
+    main_window = MainWindow(widgets, drive, data)
+    # Instance of FlashScreen class
+    flash_screen = FlashScreen(widgets, drive, data)
+    # Adding main_window widget & flash_screen widget to MAIN STACKED WIDGET
     widgets.addWidget(main_window)
     widgets.addWidget(flash_screen)
+    # Setting up main_window properties common for both main_window & flash_screen widget
     set_window_properties(widgets)
+    # Starting & ending of app
     widgets.show()
     exit(application.exec())
