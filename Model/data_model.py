4b8406141f7da560a7f6cd6aaa48b235f07f1246
Naman Jain <Naman.Jain1419@gmail.com>
2021-10-12 16:21:01 +0530

    Created and added data_model.py which includes the important data regarding iso file and drive for booting the usb drive.
    This file shares the data to all the modules with single object created in main.py file.

---
+++ Model/data_model.py
@@ -0,0 +1,43 @@
+class Data:
+
+    def __init__(self):
+
+        self.__file_name = None
+        self.__file_path = None
+        self.__drive_name = None
+        self.__drive_path = None
+        self.__drive_device = None
+
+    # Setters of Data Model
+
+    def set_file_name(self, file_name):
+        self.__file_name = file_name
+
+    def set_file_path(self, file_path):
+        self.__file_path = file_path
+
+    def set_drive_name(self, drive_name):
+        self.__drive_name = drive_name
+
+    def set_drive_path(self, drive_path):
+        self.__drive_path = drive_path
+
+    def set_drive_device(self, drive_device):
+        self.__drive_device = drive_device
+
+    # Getters of Data Model
+
+    def get_file_name(self):
+        return self.__file_name
+
+    def get_file_path(self):
+        return self.__file_path
+
+    def get_drive_name(self):
+        return self.__drive_name
+
+    def get_drive_path(self):
+        return self.__drive_path
+
+    def get_drive_device(self):
+        return self.__drive_device
