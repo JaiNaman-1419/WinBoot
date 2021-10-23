4fa1b39a7428c27e4d541f16bba050a077564d02
Naman Jain <Naman.Jain1419@gmail.com>
2021-10-23 20:12:13 +0530

    Created update_progress_bar() & calculate_copied_size() method as callback method for copytree() and copyfileobj() method in replicate.py.
    Also removed TODO comments of completed tasks.

--- src/flash_usb.py
+++ src/flash_usb.py
@@ -8,8 +8,6 @@ from shutil import copyfile, ignore_patterns
 
 class FlashUSB:
 
-    # TODO create method which copy all files excluding install.wim to usb drive.
-
     def __init__(self, data, button_obj):
         self.__data = data
         self.__dir = False
@@ -58,7 +56,17 @@ class FlashUSB:
             # TODO show dialog box that an error occurred!
             print(f"\n\nLine Number: 47\nError occurred: {e}")
 
-    def __copy_items_to_usb(self):
+    def __calculate_copied_size(self):
+        replica = Replica()
+        folder_size = replica.get_folder_size(self.__data.get_drive_path())
+        iso_file_size = replica.get_folder_size(self.__data.get_iso_mount_point())
+        status = "{:.2f}".format((folder_size / iso_file_size) * 100)
+        return status
+
+    def __update_progress_bar(self, flash_screen):
+        flash_screen.flash_bar.setValue(self.__calculate_copied_size())
+
+    def __copy_items_to_usb(self, flash_screen):
         src = self.__data.get_file_path()
         dst = self.__data.get_iso_mount_point()
         replica = Replica()
@@ -69,26 +77,26 @@ class FlashUSB:
 
             if isfile(item):
                 copyfile(item, join(dst, item_name))
-
+                self.__update_progress_bar(flash_screen)
             else:
-                replica.copytree(src=item, dst=join(dst, item_name), callback=None,
-                                 ignore=ignore_patterns("install.wim"))
+                replica.copytree(src=item, dst=join(dst, item_name), flash_screen=flash_screen,
+                                 callback=self.__update_progress_bar, ignore=ignore_patterns("install.wim"))
 
         del replica
 
-    def __copy_swm_file_to_usb(self):
+    def __copy_swm_file_to_usb(self, flash_screen):
         replica = Replica()
         for file in ["install.swm", "install2.swm"]:
             src = join("/media", file)
             dst = join(self.__data.get_iso_mount_point(), "sources", file)
-            replica.copyfileobj(fsrc=src, fdst=dst, callback=None)
+            replica.copyfileobj(fsrc=src, fdst=dst, flash_screen=flash_screen, callback=self.__update_progress_bar)
 
         del replica
 
-    def start_flash(self):
+    def start_flash(self, flash_screen):
         self.__mount_iso_file()
-        self.__copy_items_to_usb()
+        self.__copy_items_to_usb(flash_screen)
         self.__convert_wim_to_swm()
-        self.__copy_swm_file_to_usb()
+        self.__copy_swm_file_to_usb(flash_screen)
         self.__unmount_iso_file()
         self.__remove_directory()
