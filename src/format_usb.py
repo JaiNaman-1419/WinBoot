9a42461b9175a8653de03aa8f3c394044f13f6ca
Naman Jain <Naman.Jain1419@gmail.com>
2021-11-13 08:06:09 +0530

    Changed the command of __format_new_partition().

--- src/format_usb.py
+++ src/format_usb.py
@@ -1,4 +1,4 @@
-from os import getcwd
+from os import getcwd, system
 from subprocess import check_output
 
 
@@ -14,26 +14,30 @@ class FormatUSB:
     # sudo parted -s -a optimal -- /dev/sdb mkpart primary fat32 1MiB 100%
     # sudo parted -s -- /dev/sdb align-check optimal 1
     # sudo mkfs.vfat -F32 /dev/sdb1
-    # sudo mkfs.vfat -F 32 -n 'name_for_your_pendrive' /dev/sdy1
+    # sudo mkfs.fat -F32 -n 'name_for_your_pendrive' /dev/sdy1
+
+    def __unmount_usb_patitions(self):
+        system(f"umount {self.__USB_DEVICE[:-1]}?")
 
     def __erase_partition_signature(self):
-        check_output(["sudo", "wipefs", "--all", "--force", f"{self.__USB_DEVICE[:-1]}"])
+        check_output(["wipefs", "--all", "--force", f"{self.__USB_DEVICE[:-1]}"])
 
     def __create_partition_table(self):
-        check_output(["sudo", "parted", "-s", "-a", "optimal", "--", f"{self.__USB_DEVICE[:-1]}", "mklabel", "gpt"])
+        check_output(["parted", "-s", "-a", "optimal", "--", f"{self.__USB_DEVICE[:-1]}", "mklabel", "gpt"])
 
     def __create_new_partition(self):
         check_output(
-            ["sudo", "parted", "-s", "-a", "optimal", "--", f"{self.__USB_DEVICE[:-1]}", "mkpart", "primary", "fat32",
+            ["parted", "-s", "-a", "optimal", "--", f"{self.__USB_DEVICE[:-1]}", "mkpart", "primary", "fat32",
              "1MiB", "100%"])
 
     def __disk_align_check(self):
-        check_output(["sudo", "parted", "-s", "--", f"{self.__USB_DEVICE[:-1]}", "align-check", "optimal", "1"])
+        check_output(["parted", "-s", "--", f"{self.__USB_DEVICE[:-1]}", "align-check", "optimal", "1"])
 
     def __format_new_partition(self):
-        check_output(["sudo", "mkfs.vfat", "-F32", "-n", f"{self.__USB_LABEL}", f"{self.__USB_DEVICE}"])
+        check_output(["mkfs.fat", "-F32", "-n", f"{self.__USB_LABEL}", f"{self.__USB_DEVICE}"])
 
     def format_usb_drive(self):
+        self.__unmount_usb_patitions()
         self.__erase_partition_signature()
         self.__create_partition_table()
         self.__create_new_partition()
