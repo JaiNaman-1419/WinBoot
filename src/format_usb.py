460778778a1176ed16f7d0627448b57316d0f402
Naman Jain <Naman.Jain1419@gmail.com>
2021-11-17 22:11:06 +0530

    Now using progress bar for formatting the USB drive also.

--- src/format_usb.py
+++ src/format_usb.py
@@ -1,5 +1,6 @@
-from os import getcwd, system
+from os.path import exists
 from subprocess import check_output
+from os import getcwd, system, mkdir
 
 
 class FormatUSB:
@@ -8,13 +9,11 @@ class FormatUSB:
         self.__working_directory = getcwd()
         self.__USB_LABEL = "WINDOWS"
         self.__USB_DEVICE = data.get_drive_device()
+        self.__USB_PATH = data.get_drive_path()
 
-    # sudo wipefs --all --force /dev/sdb
-    # sudo parted -s -a optimal -- /dev/sdb mklabel msdos
-    # sudo parted -s -a optimal -- /dev/sdb mkpart primary fat32 1MiB 100%
-    # sudo parted -s -- /dev/sdb align-check optimal 1
-    # sudo mkfs.vfat -F32 /dev/sdb1
-    # sudo mkfs.fat -F32 -n 'name_for_your_pendrive' /dev/sdy1
+    def __create_directory(self, directory):
+        if not exists(directory):
+            mkdir(directory)
 
     def __unmount_usb_patitions(self):
         system(f"umount {self.__USB_DEVICE[:-1]}?")
@@ -36,10 +35,29 @@ class FormatUSB:
     def __format_new_partition(self):
         check_output(["mkfs.fat", "-F32", "-n", f"{self.__USB_LABEL}", f"{self.__USB_DEVICE}"])
 
-    def format_usb_drive(self):
+    def __mount_formatted_drive(self):
+        self.__create_directory(self.__USB_PATH)
+        check_output(["sudo", "mount", self.__USB_DEVICE, self.__USB_PATH])
+
+    def format_usb_drive(self, flash_screen):
         self.__unmount_usb_patitions()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 1.00)
+        flash_screen.flash_bar.setValue(1.00)
         self.__erase_partition_signature()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 2.00)
+        flash_screen.flash_bar.setValue(2.00)
         self.__create_partition_table()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 3.00)
+        flash_screen.flash_bar.setValue(3.00)
         self.__create_new_partition()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 4.00)
+        flash_screen.flash_bar.setValue(4.00)
+        flash_screen.flash_bar.setFormat('%.02f%%' % 5.00)
+        flash_screen.flash_bar.setValue(5.00)
         self.__disk_align_check()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 6.00)
+        flash_screen.flash_bar.setValue(6.00)
         self.__format_new_partition()
+        self.__mount_formatted_drive()
+        flash_screen.flash_bar.setFormat('%.02f%%' % 7.00)
+        flash_screen.flash_bar.setValue(7.00)
